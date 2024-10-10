737a781564f0be5ff8be21c48d4f65d22e3ae88b
Blake-Ballew <Blake.Ballew@gatech.edu>
2024-10-09 21:40:11 -0400

    added semicolon to struct definitions

--- src/DataDefs/DrawCommand.h
+++ src/DataDefs/DrawCommand.h
@@ -24,33 +24,33 @@ namespace TesseractCommon
     struct DrawCommand
     {
         uint8_t opcode; // 6 bits
-    }
+    };
 
     struct SetStripZLevel
     {
         uint8_t zLevel; // 8 bits
-    }
+    };
 
     struct SetTimingOffset
     {
         uint32_t offsetNanoseconds; // 24 bits
-    }
+    };
 
     struct SetTimingScale
     {
         uint32_t timeScaleFactor; // 24 bits
-    }
+    };
 
     struct ClearMemory
     {
         uint16_t padding; // 10 bits
-    }
+    };
 
     struct SetZLevelQuad
     {
         uint8_t quadrant; // 2 bits
         uint8_t zLevel; // 8 bits
-    }
+    };
 
     struct SetPaletteColor
     {
@@ -59,7 +59,7 @@ namespace TesseractCommon
         uint8_t red; // 8 bits
         uint8_t green; // 8 bits
         uint8_t blue; // 8 bits
-    }
+    };
 
     struct DrawZOrderPixels
     {
@@ -67,7 +67,7 @@ namespace TesseractCommon
         uint16_t zStart; // 16 bits
         uint16_t zEnd; // 16 bits
         uint8_t color; // 8 bits
-    }
+    };
 
     struct DrawXYPixel
     {
@@ -75,7 +75,7 @@ namespace TesseractCommon
         uint8_t xPos; // 8 bits
         uint8_t yPos; // 8 bits
         uint8_t color; // 8 bits
-    }
+    };
 
     struct DrawRect
     {
@@ -85,5 +85,5 @@ namespace TesseractCommon
         uint8_t width; // 8 bits
         uint8_t height; // 8 bits
         uint8_t color; // 8 bits
-    }
+    };
 };
