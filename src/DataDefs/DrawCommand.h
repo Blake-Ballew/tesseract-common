eb9fc1ca76203942b8757baa5f0642daa162501d
Blake-Ballew <Blake.Ballew@gatech.edu>
2024-10-09 21:33:32 -0400

    added draw command definitions

--- src/DataDefs/DrawCommand.h
+++ src/DataDefs/DrawCommand.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Arduino.h>
+
 namespace TesseractCommon
 {
     // Opcodes for GPU commands. 6 bits
@@ -19,5 +21,69 @@ namespace TesseractCommon
 
     } DrawCommandOpcode;
 
-    
+    struct DrawCommand
+    {
+        uint8_t opcode; // 6 bits
+    }
+
+    struct SetStripZLevel
+    {
+        uint8_t zLevel; // 8 bits
+    }
+
+    struct SetTimingOffset
+    {
+        uint32_t offsetNanoseconds; // 24 bits
+    }
+
+    struct SetTimingScale
+    {
+        uint32_t timeScaleFactor; // 24 bits
+    }
+
+    struct ClearMemory
+    {
+        uint16_t padding; // 10 bits
+    }
+
+    struct SetZLevelQuad
+    {
+        uint8_t quadrant; // 2 bits
+        uint8_t zLevel; // 8 bits
+    }
+
+    struct SetPaletteColor
+    {
+        uint16_t futureUse; // 10 bits
+        uint8_t colorIdx; // 8 bits
+        uint8_t red; // 8 bits
+        uint8_t green; // 8 bits
+        uint8_t blue; // 8 bits
+    }
+
+    struct DrawZOrderPixels
+    {
+        uint8_t drawMode; // 2 bits
+        uint16_t zStart; // 16 bits
+        uint16_t zEnd; // 16 bits
+        uint8_t color; // 8 bits
+    }
+
+    struct DrawXYPixel
+    {
+        uint8_t drawMode; // 2 bits
+        uint8_t xPos; // 8 bits
+        uint8_t yPos; // 8 bits
+        uint8_t color; // 8 bits
+    }
+
+    struct DrawRect
+    {
+        uint8_t drawMode; // 2 bits
+        uint8_t xPos; // 8 bits
+        uint8_t yPos; // 8 bits
+        uint8_t width; // 8 bits
+        uint8_t height; // 8 bits
+        uint8_t color; // 8 bits
+    }
 };
